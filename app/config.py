42e8b26a27e08c765237b51d052bb34b3aece7cb
kwanimaa <akousakwanimaaaa@gmail.com>
2026-01-05 23:25:52 +0000

    AUTHENTICATION SYSTEM

--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
 class Config:
     SQLALCHEMY_DATABASE_URI="sqlite:///photovault.db"
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+    
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
+from extensions import db
+
 class User(db.Model):
     __tablename__ = "users"
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), nullable=False, unique=True)
     email = db.Column(db.String(45), nullable=False, unique=True)
