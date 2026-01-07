8fe0a20183081691662d001c519a8f5d7d6b4798
kwanimaa <akousakwanimaaaa@gmail.com>
2026-01-07 23:28:51 +0000

    small fixes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Photo-vault-api
+#couldinary 
--- app/auth_routes.py
+++ app/auth_routes.py
@@ -1,7 +1,4 @@
-from flask import request,jsonify,Blueprint
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from flask_bcrypt import Bcrypt
-from .models import db, User
+
 
 bcrypt = Bcrypt()
 auth_bp = Blueprint("auth_bp", __name__, url_prefix="/auth")
