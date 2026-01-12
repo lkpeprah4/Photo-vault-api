38c2c9f1d736d35bf505ef516ecbf02b0f2c4034
kwanimaa <akousakwanimaaaa@gmail.com>
2026-01-12 05:58:30 +0000

    streak activated

--- app/auth_routes.py
+++ app/auth_routes.py
@@ -10,7 +10,7 @@ from models import User
 auth_bp = Blueprint("auth_bp", __name__, url_prefix="/auth")
 
 
-bcrypt = breakcrypt()
+bcrypt = Bcrypt()
 auth_bp = Blueprint("auth_bp", __name__, url_prefix="/auth")
 
 
